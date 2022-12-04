2233ad15ecdf718c8e7a092ae320b7358a2c7d4d
elena-kolos <anele.solok@gmail.com>
2022-12-04 13:08:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ setup(
     name='bot_assistant',
     version='1.0',
     description='To assist your wishes',
-    url='https://github.com/kpi-donar/Bot_Assistant.git',
+    url='https://github.com/elena-kolos/ProjectPythonCoreBot4Mates.git',
     author='Bot4mates',
-    author_email='https://don-ar.github.io/',
+    author_email='https://elena-kolos.github.io/',
     license='MIT',
     packages=find_namespace_packages(),
-    install_requires=['markdown','colorama','python-dateutil','fuzzywuzzy','python-Levenshtein'],
+    install_requires=['markdown', 'colorama',
+                      'python-dateutil', 'fuzzywuzzy', 'python-Levenshtein'],
     entry_points={'console_scripts': ['bot_assist = bot4mates.main:main']}
 )
